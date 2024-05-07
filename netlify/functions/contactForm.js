27c56087e06c3541d47b62dd8bcd03c3bd55bd39
Ahemao <andersonhenao329@gmail.com>
2024-05-06 20:09:51 -0500

    finished

--- netlify/functions/contactForm.js
+++ netlify/functions/contactForm.js
@@ -4,14 +4,15 @@ config();
 
 
 const transport = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  host: process.env.EMAIL_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
 
   function enviarMail(mail) {
     return new Promise((resolve, reject) => {
