b6c43b06ea3ef55f847887eded74acb42e46aec4
Ahemao <andersonhenao329@gmail.com>
2024-05-09 22:07:33 -0500

    img seo

--- src/app/pages/likes/likes.component.ts
+++ src/app/pages/likes/likes.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-likes',
   templateUrl: './likes.component.html',
-  styleUrl: './likes.component.css'
+  styleUrls: ['./likes.component.css']
 })
 export class LikesComponent {
+  openModal(imgSrc: string) {
+    const modal = document.getElementById('modal');
+    const imgElement = document.getElementById('img01') as HTMLImageElement | null;
 
+    if (modal && imgElement) {
+      modal.style.display = 'block';
+      imgElement.src = imgSrc;
+    }
+  }
+
+  closeModal() {
+    const modal = document.getElementById('modal');
+    if (modal) {
+      modal.style.display = 'none';
+    }
+  }
 }
